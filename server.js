03e3328120e0173161d8acbe9c31cbc39d23c2bf
Adrian J <godeatingtree@gmail.com>
2020-02-10 16:16:26 +0100

    added caching

--- server.js
+++ server.js
@@ -2,28 +2,41 @@ import express from 'express';
 const app = express();
 import https from 'https';
 import axios from 'axios';
-
+import fs from 'fs';
 
 app.route('/api/:path/:id').get((req, res) => {
   console.log(req.params);
-    axios.get('https://mhw-db.com/' + req.params["path"] + "/" + req.params["id"] )
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+  axios.get('https://mhw-db.com/' + req.params["path"] + "/" + req.params["id"])
+    .then(response => {
+      console.log(response.data);
+      res.send(response.data)
+    })
+    .catch(error => {
+      console.log(error);
+    });
 })
 
-// app.route('/api/:path').get((req, res) => {
-//     axios.get('https://mhw-db.com/' + req.params["path"] )
-//       .then(response => {
-//         console.log(response.data);
-//         res.end(respo.data);
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-// })
+app.route('/api/:path').get((req, res) => {
 
-app.listen(8000, () => { console.log('Server started')});
+  var path = './cache/' + req.params["path"]
+  if (fs.existsSync(path)) {
+    var file = fs.readFileSync(path)
+    console.log(file);
+    
+    file = JSON.parse(file);
+    res.send(file);
+  }
+  else {
+    axios.get('https://mhw-db.com/' + req.params["path"])
+      .then(response => {
+        //console.log(response.data);
+        fs.writeFile(path, JSON.stringify(response.data), () => { })
+        res.send(response.data);
+      })
+      .catch(error => {
+        //console.log(error);
+      });
+  }
+})
+
+app.listen(8000, () => { console.log('Server started') });
