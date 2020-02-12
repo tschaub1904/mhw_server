13a2783b8b38574f4c12fcbf04a0b2081653487e
Adrian J <godeatingtree@gmail.com>
2020-02-12 16:25:00 +0100

    added search route, doesn't work

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ import https from 'https';
 import axios from 'axios';
 import fs from 'fs';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import mongodb from 'mongodb'
+const MongoClient = mongodb.MongoClient;
+let dbUrl = "mongodb://localhost:27017/"
+
+app.use(bodyParser.json())
 
 app.route('/api/:path/:id').get((req, res) => {
   console.log(req.params);
@@ -17,13 +24,12 @@ app.route('/api/:path/:id').get((req, res) => {
     });
 })
 
-app.route('/api/:path').get(cors(corsOptions),(req, res) => {
+app.route('/api/:path').get(cors(corsOptions), (req, res) => {
 
   var path = './cache/' + req.params["path"]
   if (fs.existsSync(path)) {
-    var file = fs.readFileSync(path)
-    console.log(file);
-    
+    var file = fs.readFileSync(path);
+
     file = JSON.parse(file);
     res.send(file);
   }
@@ -40,11 +46,30 @@ app.route('/api/:path').get(cors(corsOptions),(req, res) => {
   }
 })
 
-var corsOptions = {
-  origin: 'http://example.com',
-  optionsSuccessStatus: 200
-}
+app.route('/api/search').post(cors(), (req, res) => {
+  MongoClient.connect(dbUrl, (err, db) => {
+    if (err) throw err;
+    var dbo = db.db('mhwDatabase');
+    //////////////////////
+    var regex = "/\\b" + req.body.name + "\\b/";
+    console.log(regex)
+    //WIE FUNKTIONIERT DIESE FUCKING REGEX IN JS
+    dbo.collection('search').findOne({
+      name: req.body.name
+    }, (err, result) => {
+      console.log(result)
+      res.send(result);
+      res.end();
+    });
+    
+  })
+});
 
-app.use(cors(corsOptions))
+  var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200
+  }
 
-app.listen(8000, () => { console.log('Server started') });
+  app.use(cors())
+
+  app.listen(8000, () => { console.log('Server started') });
